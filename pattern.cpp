68238ee6d774c11654eee4f5f8cac8048536728f
18rajritik <20ritikraj04@gmail.com>
2026-04-02 23:52:53 +0530

    printing of the diamond shape

--- pattern.cpp
+++ pattern.cpp
@@ -143,17 +143,48 @@
 //     return 0;
 // }
 
-// NUMBER TRIANGLE PATTERN
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=5;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout << i << " ";
+
+//         }
+//         cout << endl;
+
+//         }
+//     }
+
+// diamond printing pattern
+
 #include<iostream>
 using namespace std;
 int main(){
-    int n=5;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout << i << " ";
+    int n=3;
+        // Implement the pattern printing logic here
+        for(int i=1;i<=n+1;i++){
+            if(i<=(n+1)/2){
+                for(int j=1;j<=(2*i-1);j++){
+                    cout << "*";
+                }
+            }
+            else if (i>(n+1)/2){
+                int k =2*i-3;
+                while(k>=1){
+                    cout << "*";
+                    k=k-2;
 
+                }              
+            
+            }
+            cout << endl;
+            
         }
-        cout << endl;
+    
 
-        }
-    }
+return 0;
+}
+
